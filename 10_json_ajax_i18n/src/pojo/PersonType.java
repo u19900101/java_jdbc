3ef78618cbbaa84920847fa62f60d7d0520ae5c3
u19900101 <815000342@qq.com>
2021-01-09 21:31:35 +0800

    json与Bean、list、map之间的转换

---
+++ 10_json_ajax_i18n/src/pojo/PersonType.java
@@ -0,0 +1,12 @@
+package pojo;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+
+/**
+ * @author lppppp
+ * @create 2021-01-09 20:48
+ */
+public class PersonType extends TypeToken<ArrayList<Person>> {
+}
