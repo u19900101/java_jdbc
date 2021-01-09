1c8b331b39d664c4f1fd66c3cd8e804547e44534
u19900101 <815000342@qq.com>
2021-01-09 16:56:16 +0800

    test git

--- books/src/test/BookServiceImplTest.java
+++ books/src/test/BookServiceImplTest.java
@@ -12,6 +12,7 @@ import pojo.Book;
 import service.impl.BookServiceImpl;
 
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -28,30 +29,30 @@ public class BookServiceImplTest {
     Book book = new Book(20,"pppppp人月神话" , "刚哥" ,
             new BigDecimal(88.15) , 20 , 80 , "static/img/default.jpg");
     @Test
-    public void addBook() {
+    public void addBook() throws SQLException {
         bookService.addBook(book);
     }
 
     @Test
-    public void updateBookById() {
+    public void updateBookById() throws SQLException {
         bookService.updateBookById(book);
     }
 
     @Test
-    public void deleteBook() {
+    public void deleteBook() throws SQLException {
 
         int i = bookService.deleteBookById(21);
         System.out.println(i);
     }
 
     @Test
-    public void queryBookById() {
+    public void queryBookById() throws SQLException {
         Book book = bookService.queryBookById(10);
         System.out.println(book);
     }
 
     @Test
-    public void queryBooks() {
+    public void queryBooks() throws SQLException {
         List<Book> books = bookService.queryBooks();
         books.forEach(System.out::println);
     }
