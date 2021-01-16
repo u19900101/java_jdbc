ad3aba24a777639fb2b5759849a29c9ae4ffd5ae
u19900101 <815000342@qq.com>
2021-01-16 10:54:41 +0800

    spring改写

--- books/src/dao/impl/OrderDaoImpl.java
+++ books/src/dao/impl/OrderDaoImpl.java
@@ -2,6 +2,10 @@ package dao.impl;
 
 import dao.BaseDao;
 import dao.OrderDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pojo.Order;
 import pojo.OrderItem;
 import utils.DBUtils;
@@ -12,13 +16,15 @@ import java.sql.Connection;
  * @author lppppp
  * @create 2021-01-08 15:15
  */
+@Repository
+@Transactional
 public class OrderDaoImpl extends BaseDao implements OrderDao {
-
+    @Autowired
+    public JdbcTemplate jdbcTemplate;
     @Override
     public void createOrder(Order order) {
-        Connection conn = DBUtils.getConn();
         String sql ="INSERT INTO t_order(id,create_time,`count`,`totalPrice`,`user_id`) VALUES(?,?,?,?,?)";
-        int update = update(conn, sql,order.getId(),order.getCreate_time(),
+        int update = jdbcTemplate.update(sql,order.getId(),order.getCreate_time(),
                 order.getCount(),order.getTotalPrice(),order.getUser_id());
         if(update>0){
             System.out.println("添加了订单");
@@ -27,11 +33,11 @@ public class OrderDaoImpl extends BaseDao implements OrderDao {
 
     @Override
     public void createOrderItem(OrderItem orderItem) {
-        Connection conn = DBUtils.getConn();
+
         String sql ="INSERT INTO t_orderItem"
         +"(`name`,`count`,`price`,`totalPrice`,`order_id`)"
         +"VALUES(?,?,?,?,?)";
-        int update = update(conn, sql,orderItem.getName(),orderItem.getCount()
+        int update = jdbcTemplate.update(sql,orderItem.getName(),orderItem.getCount()
         ,orderItem.getPrice(),orderItem.getTotalPrice(),orderItem.getOrder_id());
         if(update>0){
             System.out.println("添加了订单Item");
--- books/src/service/impl/OrderServiceImpl.java
+++ books/src/service/impl/OrderServiceImpl.java
@@ -2,6 +2,8 @@ package service.impl;
 
 import dao.OrderDao;
 import dao.impl.OrderDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pojo.Order;
 import pojo.OrderItem;
 import service.OrderService;
@@ -10,8 +12,10 @@ import service.OrderService;
  * @author lppppp
  * @create 2021-01-08 15:05
  */
+@Service
 public class OrderServiceImpl implements OrderService {
-    OrderDaoImpl orderDao = new OrderDaoImpl();
+    @Autowired
+    OrderDao orderDao;
     @Override
     public void createOrder(Order order) {
         orderDao.createOrder(order);
