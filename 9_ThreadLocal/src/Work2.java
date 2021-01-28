e60b623a1d364ece45f87c5606ccbaeea5175c59
u19900101 <815000342@qq.com>
2021-01-28 13:15:42 +0800

    删掉无用的module

--- 9_ThreadLocal/src/Work2.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author lppppp
- * @create 2021-01-09 9:20
- */
-public class Work2 {
-    public static void work(){
-        System.out.println("Work2__"+Thread.currentThread().getName()+"___Num is __"+Task.threadLocal.get());
-    }
-}
